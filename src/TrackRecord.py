027122f635ab85357b466196e93d5cfa9562e7cf
José Manuel Calderón Trilla <jmct@jmct.cc>
2022-06-26 00:46:27 -0400

    Fix a bug so all records are checked against default
    I accidentally made the check for the default record be _outside_ the loop
    where we iterate over all of the records for a track. This was bad!
    
    Now it properly checks each 3-byte chunk to see if it's the default.

--- src/TrackRecord.py
+++ src/TrackRecord.py
@@ -4,20 +4,22 @@ class TrackRecord:
 
     def __init__(self, bs):
 
-        if bs == default_record:
-          self.records.append(None)
-          return
         self.records = []
         for i in range(0, 18, 3):
-            a = bs[i+0] >> 4
-            b = bs[i+0] & 0b00001111
-            c = bs[i+1] >> 4
-            d = bs[i+1] & 0b00001111
-            e = bs[i+2] >> 4
-            f = bs[i+2] & 0b00001111
+            # get the current slice we're working on and see if it's the
+            # default record
+            if bs[i:i+3] == default_record:
+                record = None
+            else:
+                a = bs[i+0] >> 4
+                b = bs[i+0] & 0b00001111
+                c = bs[i+1] >> 4
+                d = bs[i+1] & 0b00001111
+                e = bs[i+2] >> 4
+                f = bs[i+2] & 0b00001111
 
-            record = {'character': e, 'time': (
-                f * 2 ** 16 + c * 2 ** 12 + d * 2 ** 8 + a * 2 ** 4 + b)*10}
+                record = {'character': e, 'time': (
+                    f * 2 ** 16 + c * 2 ** 12 + d * 2 ** 8 + a * 2 ** 4 + b)*10}
 
             self.records.append(record)
 
