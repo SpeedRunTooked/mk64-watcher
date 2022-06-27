3f8b2586c0c57d553f84fce7bc370c5af2649fa8
salvoch <github@salvoch.com>
2022-06-26 23:56:05 -0600

    Changed loop to only send mk64.py Changed the loop in mk64.py in compare_records Instead of iterating through all 5 records, we only look at the record at [0] (the top record).

--- src/utils/mk64.py
+++ src/utils/mk64.py
@@ -5,20 +5,15 @@ from src.data.game import RACER_IDS, TRACK_SLUGS
 def compare_records(original, newrecords):
 
     for j, (old_times, new_times) in enumerate(zip(original, newrecords)):
-        for i, (or_re, nr_re) in enumerate(zip(old_times['track-record'].records, new_times['track-record'].records)):
-            if i > 4:
-                break
-            # If the new record (nr_re) is `None`, this means that the user
-            # clear their game memory. If the old record is `None`, we don't
-            # care, we still want to upload the new time.
-            if or_re != nr_re and nr_re != None:
-                # Commented these out because not being used atm
-                # track_name = old_times['name']
-                # racer_name = racer_ids[nr_re['character']]
-                new_time = nr_re['time']
-                uploader.send_to_gus(new_time, TRACK_SLUGS[j], rtype="3lap")
-                print('New top 5 race time!')
-                break
+
+        three_lap_record = new_times['track-record'].records[0]
+        if old_times['track-record'].records[0] != three_lap_record and three_lap_record != None:
+            # Commented these out because not being used atm
+            # track_name = old_times['name']
+            # racer_name = racer_ids[nr_re['character']]
+            new_time = three_lap_record['time']
+            uploader.send_to_gus(new_time, TRACK_SLUGS[j], rtype='3lap')
+            print('New 3 lap record!')
 
         best_lap_record = new_times['track-record'].records[5]
         if old_times['track-record'].records[5] != best_lap_record and best_lap_record != None:
