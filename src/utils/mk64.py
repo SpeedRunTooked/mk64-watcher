6936b52dfe1c64c00fe4f5344b93e7c8d50d3bdd
salvoch <github@salvoch.com>
2022-06-27 22:18:40 -0600

    Added comments explaining the new record, removed old comment
    Added the comments explaining why we skip if new record = None
    Also deleted commented out code referencing code that is no longer needed

--- src/utils/mk64.py
+++ src/utils/mk64.py
@@ -6,20 +6,19 @@ def compare_records(original, newrecords):
 
     for j, (old_times, new_times) in enumerate(zip(original, newrecords)):
 
+        # Check the top 3-lap record, and the best lap record.
+        # If the new record is `None`, this means the user
+        # cleared thier game memory. If the old record is `None`
+        # that means no record has been set before, so we proceed
+
         three_lap_record = new_times['track-record'].records[0]
         if old_times['track-record'].records[0] != three_lap_record and three_lap_record != None:
-            # Commented these out because not being used atm
-            # track_name = old_times['name']
-            # racer_name = racer_ids[nr_re['character']]
             new_time = three_lap_record['time']
             uploader.send_to_gus(new_time, TRACK_SLUGS[j], rtype='3lap')
             print('New 3 lap record!')
 
         best_lap_record = new_times['track-record'].records[5]
         if old_times['track-record'].records[5] != best_lap_record and best_lap_record != None:
-            # Commented these out because not being used atm
-            # track_name = old_times['name']
-            # racer_name = racer_ids[nr_re['character']]
             new_time = best_lap_record['time']
             uploader.send_to_gus(new_time, TRACK_SLUGS[j], rtype='flap')
             print('New lap record!')
